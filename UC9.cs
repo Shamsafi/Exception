2b057349a3d4b06839b0c24f1d4147e3cb9e9910
Safi <mdsafi729@gmail.com>
2023-05-06 14:04:44 +0530

    [shamsuddin]Fix: Adding UC9 file

---
+++ UC9.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EmailValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] emails = { "sample@mail.com", "sample.mail.com", "sample@mail" };
+
+            foreach (string email in emails)
+            {
+                try
+                {
+                    if (!IsValidEmail(email))
+                    {
+                        throw new Exception($"Error: {email} is not a valid email address.");
+                    }
+
+                    Console.WriteLine($"Success: {email} is a valid email address.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var mailAddress = new System.Net.Mail.MailAddress(email);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
