7bf38adaced597c1684d8115478f3b5157e09ffa
Safi <mdsafi729@gmail.com>
2023-05-06 14:03:44 +0530

    [shamsuddin]Fix: Adding UC8 file

---
+++ UC8.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {  // Must have 1 special char
+        try
+        {
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            if (!IsValidPassword(password))
+            {
+                throw new Exception("Invalid password entered.");
+            }
+
+            Console.WriteLine("Valid password entered: " + password);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private static bool IsValidPassword(string password)
+    {
+
+        if (password.Length < 8)
+        {
+            return false;
+        }
+
+        if (!password.Any(char.IsUpper))
+        {
+            return false;
+        }
+
+        if (!password.Any(char.IsLower))
+        {
+            return false;
+        }
+
+        if (!password.Any(char.IsDigit))
+        {
+            return false;
+        }
+
+        int specialCharCount = password.Count(c => !char.IsLetterOrDigit(c));
+        if (specialCharCount != 1)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
