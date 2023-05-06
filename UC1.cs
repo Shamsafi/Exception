e00a7729d2105818b5f2bfe9625a24ad6acf22b2
Safi <mdsafi729@gmail.com>
2023-05-06 13:58:22 +0530

    [shamsuddin]Fix: Adding UC1 file

---
+++ UC1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class InvalidFirstNameException : Exception
+{
+    public InvalidFirstNameException() : base("Invalid first name. First name should start with a capital letter and have at least 3 characters.") { }
+}
+
+public class UC1
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            if (firstName.Length < 3 || !Char.IsUpper(firstName[0]))
+            {
+                throw new Exception();
+            }
+
+            Console.WriteLine("Valid first name entered: " + firstName);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
