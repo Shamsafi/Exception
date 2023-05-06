55e701305c23f74fe0374d74983cffb6a58fb589
Safi <mdsafi729@gmail.com>
2023-05-06 14:03:06 +0530

    [shamsuddin]Fix: Adding UC7 file

---
+++ UC7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your password:");
+            string password = Console.ReadLine();
+            //Must contain at least one numeric number
+            try
+            {
+                if (password.Length < 8)
+                {
+                    throw new Exception("Error: Password should be at least 8 characters long.");
+                }
+
+                if (!char.IsDigit(password, password.Length - 1))
+                {
+                    throw new Exception("Error: Password should end with a numeric number.");
+                }
+
+                if (char.IsDigit(password, 0) || char.IsSymbol(password, 0) || char.IsPunctuation(password, 0))
+                {
+                    throw new Exception("Error: Password should not start with a digit or special character.");
+                }
+
+                Console.WriteLine("Success: Valid password.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
